22b4916061767442003e396b731bf37a4cd349bb
Aoun Kazmi <aounabbaskazmi2005@gmail.com>
2026-07-01 12:26:37 +0500

    fixed

--- rag/vector_store.py
+++ rag/vector_store.py
@@ -2,9 +2,11 @@ from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 def create_vector_store(chunks):
-    embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embedding_model = GoogleGenerativeAIEmbeddings(model="text-embedding-004")
+    
     vector_store = Chroma.from_texts(
         texts=chunks,
         embedding=embedding_model
     )
+    
     return vector_store
